7abaa3629616c6dcb6fe2a6e4631682461cf5b50
ZSShen <andy780328@hotmail.com>
2014-05-31 10:20:37 -0700

    Complete basic features.

--- Array/DynamicArray/dynamic_array.c
+++ Array/DynamicArray/dynamic_array.c
@@ -3,24 +3,24 @@
 /*===========================================================================*
  *                  Simulation for private variables                         *
  *===========================================================================*/
-static unsigned long ulCapacity, ulSize;
+static unsigned long _ulCapacity;
 
 
 /*===========================================================================*
  *                Implementation for exported functions                      *
  *===========================================================================*/
-bool DArrayInit(DynamicArray *self) {
+bool DArrayInit(DynamicArray *self, unsigned long ulCapacity) {
 
-    ulCapacity = 1;
-    ulSize = 0;
-    self->array = (void**)malloc(sizeof(void*) * ulCapacity);    
+    _ulCapacity = ulCapacity;
+    self->array = (void**)malloc(sizeof(void*) * _ulCapacity);    
     if (self->array == NULL)
         return false;
     
     /* Let the function pointers point to the corresponding functions. */
     self->put = DArrayPut;
     self->get = DArrayGet;
-    self->size = DArraySize;
+    self->resize = DArrayResize;
+    self->capacity = DArrayCapacity;
 
     return true;
 }
@@ -33,10 +33,61 @@ void DArrayDeinit(DynamicArray *self) {
 }
 
 
-bool DArrayPut(DynamicArray *self, void *pItem, unsigned long ulIndex);
+/**
+ * DArrayPut(): Insert an item to the designated position.
+ */
+bool DArrayPut(DynamicArray *self, void *pItem, unsigned long ulIndex) {
+
+    /* Reject the invalid index*/
+    if (ulIndex >= _ulCapacity)
+        return false;
+
+    self->array[ulIndex] = pItem;
+    return true;
+}
 
 
-void* DArrayGet(DynamicArray *self, unsigned long ulIndex);
+/**
+ * DArrayGet(): Retrieve an item from the designated position.
+ */
+void* DArrayGet(DynamicArray *self, unsigned long ulIndex) {
+
+    /* Reject the invalid index. */
+    if (ulIndex >= _ulCapacity)
+        return NULL;
+    
+    return self->array[ulIndex];
+}
 
 
-unsigned long DArraySize(DynamicArray *self);
+/**
+ * DArrayResize(): Resize the array with the specified number of times.
+ */
+bool DArrayResize(DynamicArray *self, double udTimes) {
+    unsigned long   ulNewCapacity;    
+    void            **new;
+
+    ulNewCapacity = _ulCapacity * udTimes;
+    if (ulNewCapacity <= 0)
+        return false;
+
+    new = realloc(self->array, ulNewCapacity);
+    if (new == NULL)
+        return false;
+
+    self->array = new;
+    _ulCapacity = ulNewCapacity;
+
+    return true;
+}
+
+
+/**
+ * DArraySize(): Return the size of the array.
+ */
+unsigned long DArrayCapacity(DynamicArray *self) {
+
+    return _ulCapacity;
+}
+
+
--- Array/DynamicArray/dynamic_array.h
+++ Array/DynamicArray/dynamic_array.h
@@ -6,10 +6,10 @@
 #include <stdbool.h>
 
 /* Wrapper for dynamic array initialization. */
-#define DynamicArray_init(p)        p = malloc(sizeof(DynamicArray)); \
+#define DynamicArray_init(p, q)     p = malloc(sizeof(DynamicArray)); \
                                     if (p != NULL) { \
                                         bool check; \
-                                        check = DArrayInit(p); \
+                                        check = DArrayInit(p, q); \
                                         if (check == false) { \
                                             free(p); \
                                             p = NULL; \
@@ -20,6 +20,7 @@
 /* Wrapper for dynamic array deinitialization. */
 #define DynamicArray_deinit(p)      if (p != NULL) { \
                                         DArrayDeinit(p); \
+                                        free(p); \
                                         p = NULL; \
                                     }
 
@@ -27,27 +28,76 @@
 typedef struct _DynamicArray {
     void **array;
 
-    bool          (*put) (struct _DynamicArray*, void*, unsigned long);
-    void*         (*get) (struct _DynamicArray*, unsigned long);    
-    unsigned long (*size)(struct _DynamicArray*);
+    bool          (*put)      (struct _DynamicArray*, void*, unsigned long);
+    void*         (*get)      (struct _DynamicArray*, unsigned long);    
+    bool          (*resize)   (struct _DynamicArray*, double);
+    unsigned long (*capacity) (struct _DynamicArray*);
 } DynamicArray;
 
 
-/* Constructor for dynamic array. */
-bool DArrayInit(DynamicArray *self);
+/**
+ * This function is the constructor of DynamicArray structure.
+ * @param self          The pointer to the DynamicArray structure.
+ * @param ulCapacity    The initial capacity of the requested array. 
+ *
+ * @return              true : The array is successfully allocated.
+ *                      false: The array cannot be allocated due to insufficient memory space.
+ */
+bool DArrayInit(DynamicArray *self, unsigned long ulCapacity);
 
 
-/* Destructor for dynamic array. */
+/*
+ * This function is the destructor of DynamicArray structure.
+ *
+ * @param self          The pointer to the DynamicArray structure.
+ */
 void DArrayDeinit(DynamicArray *self);
 
 
+/**
+ * This function inserts an item to the designated position.
+ * 
+ * @param self          The pointer to the DynamicArray structure.
+ * @param pItem         The pointer to the item which is to be inserted.
+ * @param ulIndex       The designated position.   
+ *
+ * @return              true : The item is inserted successfully.
+ *                      false: The item cannot be inserted due to insufficient memory space.
+ */
 bool DArrayPut(DynamicArray *self, void *pItem, unsigned long ulIndex);
 
 
+/**
+ * This function retrives an item from the designated position.
+ * @param self          The pointer to the DynamicArray structure.
+ * @param ulIndex       The designated position.
+ *
+ * @return              Non-NULL: The requested item.
+ *                      NULL    : The item cannot be retrieved due to illegally designated position.
+ */
 void* DArrayGet(DynamicArray *self, unsigned long ulIndex);
 
 
-unsigned long DArraySize(DynamicArray *self);
+/**
+ * This function resize the array with the specified number of times.
+ * Note: The number of times should be larger than zero.
+ *
+ * @param self          The pointer to the DynamicArray structure.
+ * @param udTimes       The specified number of times.
+ * 
+ * @return              true : The array is resized successfully.
+ *                      false: The array cannot be resized due to insufficient memory space.
+ */
+bool DArrayResize(DynamicArray *self, double udTimes);
+
+
+/**
+ * This function returns the capacity of the array.
+ * @param self          The pointer to the DynamicArray structure.
+ * 
+ * @return              The capacity.
+ */
+unsigned long DArrayCapacity(DynamicArray *self);
 
 
 #endif
