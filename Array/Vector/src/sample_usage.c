69fb5db7bd990104c61b0af0a275e925334daf84
ZSShen <andy780328@hotmail.com>
2015-03-26 07:23:02 -0700

    Vector: Show the non-primitive test suit.

--- Array/Vector/src/sample_usage.c
+++ Array/Vector/src/sample_usage.c
@@ -14,10 +14,7 @@
 
 typedef struct RECORD_ {
     int32_t *pKey;
-    struct VALUE_ {
-        int32_t iMajor;
-        int32_t iMinor;
-    } *pValue;
+    int32_t *pValue;
 } RECORD;
 
 
@@ -27,14 +24,8 @@ void test_primitive();
 /* The testing suit which inserts pointers to structures into the vector. */
 void test_non_primitive();
 
-/* The item comparator for integer type. */
-int32_t func_compare_primitive(Item, Item);
-
 /* The item deallocator for integer type. */
 void func_destroy_primitive(Item);
-
-/* The item comparator for type of pointer to structure. */
-int32_t func_compare_non_primitive(Item, Item);
 
 /* The item deallocator for type of pointer to structure. */
 void func_destroy_non_primitive(Item);
@@ -47,8 +38,8 @@ int main()
 
     printf("====== Test Suit for Primitive Types ======\n");
     test_primitive();
-    //printf("====== Test Suit for Non-Primitive Types ======\n");
-    //test_non_primitive();
+    printf("====== Test Suit for Non-Primitive Types ======\n");
+    test_non_primitive();
 
     return 0;
 }
@@ -81,12 +72,19 @@ void test_primitive()
 
     /* Push the first half of the elements into vector. */
     uint32_t uiRange = COUNT_CASE >> 1;
-    for (i = 0 ; i < uiRange ; i++)
-        pVector->push_back(pVector, aData[i]);
+    int32_t iRtnCode;
+    for (i = 0 ; i < uiRange ; i++) {
+        iRtnCode = pVector->push_back(pVector, aData[i]);
+        if (iRtnCode != SUCCESS)
+            goto EXIT;
+    }
 
     /* Insert the second half of the elements into vector. */
-    for (i = uiRange ; i < COUNT_CASE ; i++)
-        pVector->insert(pVector, aData[i], i - uiRange);
+    for (i = uiRange ; i < COUNT_CASE ; i++) {
+        iRtnCode = pVector->insert(pVector, aData[i], i - uiRange);
+        if (iRtnCode != SUCCESS)
+            goto EXIT;
+    }
 
     /* Check for the element identity. */
     for (i = 0 ; i < uiRange ; i++)
@@ -104,23 +102,105 @@ EXIT:
     return;
 }
 
-int32_t func_compare_primitive(Item itemSrc, Item itemTge)
+void test_non_primitive()
 {
-    int32_t iSrc, iTge;
+    /* Prepare the testing data. */
+    RECORD *aRecord = (RECORD*)malloc(sizeof(RECORD) * COUNT_CASE);
+    if (!aRecord)
+        return;
 
-#if __x86_64__
-    int64_t lTmp;
-    lTmp = (int64_t)itemSrc;
-    iSrc = (int32_t)lTmp;
-    lTmp = (int64_t)itemTge;
-    iTge = (int32_t)lTmp;
-#else
-    iSrc = (int32_t)itemSrc;
-    iTge = (int32_t)itemTge;
-#endif
+    uint32_t i, j;
+    for (i = 0 ; i < COUNT_CASE ; i++) {
+        aRecord[i].pKey = NULL;
+        aRecord[i].pValue = NULL;
+    }
 
-    return iSrc - iTge;
+    int32_t iKey;
+    for (i = 0 ; i < COUNT_CASE ; i++) {
+        iKey = rand() % SIZE_VALUE_SPACE;
+        aRecord[i].pKey = (int32_t*)malloc(sizeof(int32_t));
+        if (!(aRecord[i].pKey))
+            goto EXIT;
+        aRecord[i].pValue = (int32_t*)malloc(sizeof(int32_t));
+        if (!(aRecord[i].pValue))
+            goto EXIT;
+        *(aRecord[i].pKey) = iKey;
+        *(aRecord[i].pValue) = 0;
+    }
+
+    /* Initialize the vector. */
+    Vector *pVector;
+    int32_t iStat = VectorInit(&pVector);
+    if (iStat != SUCCESS)
+        goto EXIT;
+
+    /* Set the deallocation function. */
+    pVector->set_destroy(pVector, func_destroy_non_primitive);
+
+    /* Push the first half of the elements into vector. */
+    uint32_t uiRange = COUNT_CASE >> 1;
+    int32_t iRtnCode;
+    for (i = 0 ; i < uiRange ; i++) {
+        iRtnCode = pVector->push_back(pVector, (Item)&aRecord[i]);
+        if (iRtnCode != SUCCESS)
+            goto EXIT;
+    }
+
+    /* Insert the second half of the elements into vector. */
+    for (i = uiRange ; i < COUNT_CASE ; i++) {
+        iRtnCode = pVector->insert(pVector, (Item)&aRecord[i], i - uiRange);
+        if (iRtnCode != SUCCESS)
+            goto EXIT;
+    }
+
+    /* Check for the element identity. */
+    for (i = 0 ; i < uiRange ; i++) {
+        RECORD *pRec = (RECORD*)pVector->at(pVector, i + uiRange);
+        assert(*(aRecord[i].pKey) == *(pRec->pKey));
+    }
+
+    /* Pop the second half of the vector. */
+    for (i = 0 ; i < uiRange ; i++)
+        pVector->pop_back(pVector);
+
+    /* Check for the element identity. */
+    for (i = 0 ; i < uiRange ; i++) {
+        RECORD *pRec = (RECORD*)pVector->at(pVector, i);
+        assert(*(aRecord[i + uiRange].pKey) == *(pRec->pKey));
+    }
+
+    /* Delete the first quarter of the vector. */
+    uiRange <<= 1;
+    for (i = 0 ; i < uiRange ; i++)
+        pVector->delete(pVector, 0);
+
+DEINIT:
+    /* Deinitialize the vector. */
+    VectorDeinit(&pVector);
+
+EXIT:
+    if (aRecord) {
+        for (i = 0 ; i < COUNT_CASE ; i++) {
+            if (aRecord[i].pKey)
+                free(aRecord[i].pKey);
+            if (aRecord[i].pValue)
+                free(aRecord[i].pValue);
+        }
+        free(aRecord);
+    }
+
+    return;
 }
 
 void func_destroy_primitive(Item item) { return; }
 
+void func_destroy_non_primitive(Item item)
+{
+    RECORD *pRec = (RECORD*)item;
+    free(pRec->pKey);
+    free(pRec->pValue);
+    pRec->pKey = NULL;
+    pRec->pValue = NULL;
+
+    return;
+}
