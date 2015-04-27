a126ee9a27a87b68b72ab59ab41ade45be44d4eb
ZSShen <andy780328@hotmail.com>
2015-04-27 07:56:53 -0700

    BalancedTree: Extend the basic test suit for insert().

--- test/unit_balanced_tree.c
+++ test/unit_balanced_tree.c
@@ -56,13 +56,13 @@ void TestPrimInsertAndOrder()
 
     /**
      * The vision of the test tree after maintaining the balanced tree property.
-     *        15
-     *       /  \
-     *      9    22
-     *     / \  /  \
-     *    4  1020  25
-     *   / \
-     *  1   6
+     *          9
+     *        /   \
+     *      4      15
+     *     / \     / \
+     *    1   6   10  22
+     *         \      / \
+     *          7   20   25
      */
     CU_ASSERT(pTree->insert(pTree, (Item)10) == SUCC);
     CU_ASSERT(pTree->insert(pTree, (Item)15) == SUCC);
@@ -73,13 +73,29 @@ void TestPrimInsertAndOrder()
     CU_ASSERT(pTree->insert(pTree, (Item)6) == SUCC);
     CU_ASSERT(pTree->insert(pTree, (Item)1) == SUCC);
     CU_ASSERT(pTree->insert(pTree, (Item)4) == SUCC);
+    CU_ASSERT(pTree->insert(pTree, (Item)7) == SUCC);
 
     /* Check the item insertion and structure invariant. */
     Item item;
-    CU_ASSERT(pTree->predecessor(pTree, (Item)20, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)15);
-    CU_ASSERT(pTree->successor(pTree, (Item)20, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)22);
+    CU_ASSERT(pTree->predecessor(pTree, (Item)4, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)1);
+    CU_ASSERT(pTree->successor(pTree, (Item)4, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)6);
+
+    CU_ASSERT(pTree->predecessor(pTree, (Item)6, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)4);
+    CU_ASSERT(pTree->successor(pTree, (Item)6, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)7);
+
+    CU_ASSERT(pTree->predecessor(pTree, (Item)7, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)6);
+    CU_ASSERT(pTree->successor(pTree, (Item)7, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)9);
+
+    CU_ASSERT(pTree->predecessor(pTree, (Item)9, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)7);
+    CU_ASSERT(pTree->successor(pTree, (Item)9, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)10);
 
     CU_ASSERT(pTree->predecessor(pTree, (Item)10, &item) == SUCC);
     CU_ASSERT_EQUAL(item, (Item)9);
@@ -91,20 +107,15 @@ void TestPrimInsertAndOrder()
     CU_ASSERT(pTree->successor(pTree, (Item)15, &item) == SUCC);
     CU_ASSERT_EQUAL(item, (Item)20);
 
+    CU_ASSERT(pTree->predecessor(pTree, (Item)20, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)15);
+    CU_ASSERT(pTree->successor(pTree, (Item)20, &item) == SUCC);
+    CU_ASSERT_EQUAL(item, (Item)22);
+
     CU_ASSERT(pTree->predecessor(pTree, (Item)22, &item) == SUCC);
     CU_ASSERT_EQUAL(item, (Item)20);
     CU_ASSERT(pTree->successor(pTree, (Item)22, &item) == SUCC);
     CU_ASSERT_EQUAL(item, (Item)25);
-
-    CU_ASSERT(pTree->predecessor(pTree, (Item)9, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)6);
-    CU_ASSERT(pTree->successor(pTree, (Item)9, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)10);
-
-    CU_ASSERT(pTree->predecessor(pTree, (Item)4, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)1);
-    CU_ASSERT(pTree->successor(pTree, (Item)4, &item) == SUCC);
-    CU_ASSERT_EQUAL(item, (Item)6);
 
     /* Check the minimum and maximum item. */
     CU_ASSERT(pTree->minimum(pTree, &item) == SUCC);
@@ -113,7 +124,7 @@ void TestPrimInsertAndOrder()
     CU_ASSERT_EQUAL(item, (Item)25);
 
     /* Check the container size. */
-    CU_ASSERT_EQUAL(pTree->size(pTree), 9);
+    CU_ASSERT_EQUAL(pTree->size(pTree), 10);
 
     BalTreeDeinit(&pTree);
 }
