74658aa50a63586635129fa48bda3fe23f3fb8b4
ZSShen <andy780328@hotmail.com>
2015-05-25 20:56:47 -0700

    OrderedMap: Implement the basic unit test for constructor and destructor testing.

---
+++ test/unit_ordered_map.c
@@ -0,0 +1,60 @@
+#include "map/ordered_map.h"
+#include "CUnit/Util.h"
+#include "CUnit/Basic.h"
+
+
+/*===========================================================================*
+ *        Definition for the test cases of the primitive input suite         *
+ *===========================================================================*/
+void TestPrimPut();
+
+
+int32_t SuitePrimitive()
+{
+    CU_pSuite pSuite = CU_add_suite("Primitive Input", NULL, NULL);
+    if (!pSuite)
+        return ERR_NOMEM;
+
+    CU_pTest pTest = CU_add_test(pSuite, "Basic key value pair insertion",
+                     TestPrimPut);
+    if (!pTest)
+        return ERR_NOMEM;
+
+    return SUCC;
+}
+
+
+int32_t main()
+{
+    if (CU_initialize_registry() != CUE_SUCCESS)
+        return CU_get_error();
+    assert(CU_get_registry() != NULL);
+    assert(!CU_is_test_running());
+
+    /* Prepare the test suite for primitive input. */
+    if (SuitePrimitive() != SUCC) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    /* Launch all the tests. */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+
+    CU_cleanup_registry();
+    return SUCC;
+}
+
+
+/*===========================================================================*
+ *      Implementation for the test cases of the primitive input suite       *
+ *===========================================================================*/
+void TestPrimPut()
+{
+    OrderedMap *pMap;
+    CU_ASSERT(OdrMapInit(&pMap) == SUCC);
+
+    OdrMapDeinit(&pMap);
+}
+
+
