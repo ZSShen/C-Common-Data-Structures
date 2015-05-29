99928a84318475f2845b1251d458ba3c46017e8d
ZSShen <andy780328@hotmail.com>
2015-05-29 11:40:12 -0700

    OrderedMap: Add the demo program for advanced key value pair manipulation.

--- demo/demo_ordered_map.c
+++ demo/demo_ordered_map.c
@@ -7,9 +7,21 @@ void simple_destroy(Entry);
 void usage_simple();
 
 
+/* The example usage for advanced key value pair. */
+typedef struct Employ_ {
+    int32_t iId;
+    int8_t cYear;
+    int8_t cLevel;
+} Employ;
+int32_t advanced_compare(Entry, Entry);
+void advanced_destroy(Entry);
+void usage_advanced();
+
+
 int main()
 {
     usage_simple();
+    usage_advanced();
 
     return 0;
 }
@@ -26,7 +38,7 @@ int32_t simple_compare(Entry entSrc, Entry entTge)
 
 void simple_destroy(Entry ent)
 {
-    free((Pair*)ent);
+    free((Employ*)ent);
     return;
 }
 
@@ -38,7 +50,9 @@ void usage_simple()
     if (iRtnCode != SUCC)
         return;
 
-    /* Set the comparison and resource clean methods. */
+    /*--------------------------------------------------*
+     * Set the comparison and resource clean methods.   *
+     *--------------------------------------------------*/
     pMap->set_compare(pMap, simple_compare);
     pMap->set_destroy(pMap, simple_destroy);
 
@@ -96,6 +110,92 @@ void usage_simple()
     #else
         pMap->remove(pMap, (Key)2);
     #endif
+
+    int32_t iSize = pMap->size(pMap);
+    assert(iSize == 2);
+
+    OdrMapDeinit(&pMap);
+    return;
+}
+
+
+int32_t advanced_compare(Entry entSrc, Entry entTge)
+{
+    Pair *pPairSrc = (Pair*)entSrc;
+    Pair *pPairTge = (Pair*)entTge;
+
+    int32_t iOrder = strcmp(pPairSrc->key, pPairTge->key);
+    if (iOrder != 0)
+        return (iOrder > 0)? 1 : (-1);
+    return 0;
+}
+
+void advanced_destroy(Entry ent)
+{
+    Pair *pPair = (Pair*)ent;
+    free((Employ*)pPair->value);
+    free((Pair*)ent);
+    return;
+}
+
+void usage_advanced()
+{
+    char *aName[3] = {"Alice", "Bob", "Wesker"};
+    OrderedMap *pMap;
+
+    int32_t iRtnCode = OdrMapInit(&pMap);
+    if (iRtnCode != SUCC)
+        return;
+
+    /*--------------------------------------------------*
+     * Set the comparison and resource clean methods.   *
+     *--------------------------------------------------*/
+    pMap->set_compare(pMap, advanced_compare);
+    pMap->set_destroy(pMap, advanced_destroy);
+
+    /*--------------------------------------------------*
+     * Insert key value pairs into the map.             *
+     *--------------------------------------------------*/
+    Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 1;
+    pEmploy->cYear = 25;
+    pEmploy->cLevel = 100;
+    Pair *pPair = (Pair*)malloc(sizeof(Pair));
+    pPair->key = (Key)aName[0];
+    pPair->value = (Value)pEmploy;
+    pMap->put(pMap, (Entry)pPair);
+
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 2;
+    pEmploy->cYear = 25;
+    pEmploy->cLevel = 90;
+    pPair = (Pair*)malloc(sizeof(Pair));
+    pPair->key = (Key)aName[1];
+    pPair->value = (Value)pEmploy;
+    pMap->put(pMap, (Entry)pPair);
+
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 3;
+    pEmploy->cYear = 25;
+    pEmploy->cLevel = 80;
+    pPair = (Pair*)malloc(sizeof(Pair));
+    pPair->key = (Key)aName[2];
+    pPair->value = (Value)pEmploy;
+    pMap->put(pMap, (Entry)pPair);
+
+    /*--------------------------------------------------*
+     * Retrive the value by specifying the key.         *
+     *--------------------------------------------------*/
+    Value value;
+    pMap->get(pMap, (Key)aName[0], &value);
+    assert(((Employ*)value)->iId == 1);
+    assert(((Employ*)value)->cYear == 25);
+    assert(((Employ*)value)->cLevel == 100);
+
+    /*--------------------------------------------------*
+     * Remove the key value pair by specifying the key. *
+     *--------------------------------------------------*/
+    pMap->remove(pMap, (Key)aName[1]);
 
     int32_t iSize = pMap->size(pMap);
     assert(iSize == 2);
