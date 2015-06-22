7fd70016dadc3f4f4cbce3217ac39ce0790e8336
ZSShen <andy780328@hotmail.com>
2015-06-22 09:04:10 -0700

    Update the doxygen document.

--- doc/gen/html/balanced__tree_8h.js
+++ doc/gen/html/balanced__tree_8h.js
@@ -3,15 +3,15 @@ var balanced__tree_8h =
     [ "BalancedTree", "structBalancedTree.html", "structBalancedTree" ],
     [ "BalTreeData", "balanced__tree_8h.html#aa1bb86c6b97f263892ec7f2e1c21229d", null ],
     [ "BalTreeInit", "balanced__tree_8h.html#a51e83d01afe00f4a7ec5dbb357b61cec", null ],
-    [ "BalTreeDeinit", "balanced__tree_8h.html#a82e029f65bf6f822d2a771fac0039c00", null ],
-    [ "BalTreeInsert", "balanced__tree_8h.html#a892d165b21ea7e0b92f2e60e6df2ac96", null ],
+    [ "BalTreeDeinit", "balanced__tree_8h.html#a235663d5d389f0535fa51e3310ed1a2e", null ],
+    [ "BalTreeInsert", "balanced__tree_8h.html#acddaad9856ae3fc41c08da5df2db0f2e", null ],
     [ "BalTreeSearch", "balanced__tree_8h.html#af3911865cb73bf3ea45525426b97fa52", null ],
-    [ "BalTreeDelete", "balanced__tree_8h.html#a5f655e95e2dd596b4d5c8ceae838ed73", null ],
+    [ "BalTreeDelete", "balanced__tree_8h.html#acd14e9c38c89528f325cebe95cddd111", null ],
     [ "BalTreeMaximum", "balanced__tree_8h.html#a596d8a7f0d03ff81e75b8785c0de6d6c", null ],
     [ "BalTreeMinimum", "balanced__tree_8h.html#ad142b6cc379f0cc09235c6f0b468c49c", null ],
     [ "BalTreeSuccessor", "balanced__tree_8h.html#abab6aa3ebf18810d6075f2edc91962cd", null ],
     [ "BalTreePredecessor", "balanced__tree_8h.html#a6e71a7579736a9d6c5905229e82f6f68", null ],
     [ "BalTreeSize", "balanced__tree_8h.html#a0d9cca0ffa93caab712b7f1928e0828a", null ],
-    [ "BalTreeSetCompare", "balanced__tree_8h.html#a6e07e4891f37de1575baa784e23a0aff", null ],
-    [ "BalTreeSetDestroy", "balanced__tree_8h.html#aa233e018d343ddd162523e32dba1d3fe", null ]
+    [ "BalTreeSetCompare", "balanced__tree_8h.html#a35f67d31ffe8ca6b7cb5c7fc1efdd86b", null ],
+    [ "BalTreeSetDestroy", "balanced__tree_8h.html#a51722316496cd80a804614cb2f1e983d", null ]
 ];
--- doc/gen/html/structBalancedTree.js
+++ doc/gen/html/structBalancedTree.js
@@ -1,14 +1,14 @@
 var structBalancedTree =
 [
     [ "pData", "structBalancedTree.html#a5db575df7f25b6d5a751a101aeaf9b2c", null ],
-    [ "insert", "structBalancedTree.html#a0de44110c2ecd5f24a7b25a760c6d95d", null ],
+    [ "insert", "structBalancedTree.html#a9f0f505e2fa01b21a5e1ca572da4e462", null ],
     [ "search", "structBalancedTree.html#a93df5d804b7863e44f41651f7f273af7", null ],
-    [ "delete", "structBalancedTree.html#a364ed04f6894b331c847d1447f8af3ba", null ],
+    [ "delete", "structBalancedTree.html#aa8503a605422d0f861cbe853bbf74872", null ],
     [ "maximum", "structBalancedTree.html#a21515b0934cd4e317bfc4291410cc955", null ],
     [ "minimum", "structBalancedTree.html#adbd8ed95e039a9e2f9d078c3b66a585e", null ],
     [ "successor", "structBalancedTree.html#ae20f2b13c97531a6f46a62bdbc87664f", null ],
     [ "predecessor", "structBalancedTree.html#aa86d36b76ecb3b80b024628051e1c6e4", null ],
     [ "size", "structBalancedTree.html#afe89397bdf236912f2c1a4db33e7d0e6", null ],
-    [ "set_compare", "structBalancedTree.html#a300e8a245bce05f12876fcc0bc7b8b9a", null ],
-    [ "set_destroy", "structBalancedTree.html#a6c027aeeab3c0e6819b38ef5d9495998", null ]
+    [ "set_compare", "structBalancedTree.html#a72766d3248f386fa3a7d3095e82109f1", null ],
+    [ "set_destroy", "structBalancedTree.html#aaa051e98b5cde99b0959e4f3bb6ae2a3", null ]
 ];
