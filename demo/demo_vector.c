0d401c82e254d296936a9f9865654e24a57cb13d
ZSShen <andy780328@hotmail.com>
2015-07-25 04:29:41 -0700

    Vector: Enhance the demo program.

--- demo/demo_vector.c
+++ demo/demo_vector.c
@@ -1,54 +1,87 @@
 #include "cds.h"
 
 
-/* The example to manipulate primitive type input. */
-void usage_primitive();
+typedef struct Employ_ {
+    int8_t cYear;
+    int8_t cLevel;
+    int32_t iId;
+} Employ;
+
+
+void DestroyObject(Item item)
+{
+    free((Employ*)item);
+}
 
 
 int main()
 {
-    usage_primitive();
-
-    return 0;
-}
-
-
-void usage_primitive()
-{
     Vector *pVec;
 
-    int32_t rc = VectorInit(&pVec, 2);
+    /* You should initialize the DS before any operations. */
+    int32_t rc = VectorInit(&pVec, 1);
     if (rc != SUCC)
-        return;
+        return rc;
 
-    /* Push the item. */
-    pVec->push_back(pVec, (Item)30);
-    pVec->push_back(pVec, (Item)40);
+    /* If you plan to delegate the resource clean task to the DS, please set the
+       custom clean method. */
+    pVec->set_destroy(pVec, DestroyObject);
 
-    /* Insert the item at the designated index. */
-    pVec->insert(pVec, (Item)10, 0);
-    pVec->insert(pVec, (Item)20, 1);
+    /* Push the items. */
+    Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 3;
+    pEmploy->cLevel = 3;
+    pEmploy->cYear = 3;
+    pVec->push_back(pVec, (Item)pEmploy);
+
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 4;
+    pEmploy->cLevel = 4;
+    pEmploy->cYear = 4;
+    pVec->push_back(pVec, (Item)pEmploy);
+
+    /* Insert the items at the designated indexes. */
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 1;
+    pEmploy->cLevel = 1;
+    pEmploy->cYear = 1;
+    pVec->insert(pVec, (Item)pEmploy, 0);
+
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 2;
+    pEmploy->cLevel = 2;
+    pEmploy->cYear = 2;
+    pVec->insert(pVec, (Item)pEmploy, 1);
 
     /*---------------------------------------------------------------*
-     * Now the vector should be: [10] | [20] | [30] | [40]           *
+     * Now the vector should be: [1] | [2] | [3] | [4]               *
      *---------------------------------------------------------------*/
 
-    /* Retrieve the item with direct indexing. */
+    /* Retrieve the items with direct indexing. */
     Item item;
     pVec->get(pVec, &item, 0);
-    assert(item == (Item)10);
+    assert(((Employ*)item)->iId == 1);
     pVec->get(pVec, &item, 3);
-    assert(item == (Item)40);
+    assert(((Employ*)item)->iId == 4);
 
     /* Replace the item with direct indexing. */
-    pVec->set(pVec, (Item)100, 0);
-    pVec->set(pVec, (Item)400, 3);
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 10;
+    pEmploy->cLevel = 10;
+    pEmploy->cYear = 10;
+    pVec->set(pVec, (Item)pEmploy, 0);
+
+    pEmploy = (Employ*)malloc(sizeof(Employ));
+    pEmploy->iId = 40;
+    pEmploy->cLevel = 40;
+    pEmploy->cYear = 40;
+    pVec->set(pVec, (Item)pEmploy, 3);
 
     /*---------------------------------------------------------------*
-     * Now the vector should be: [100] | [20] | [30] | [400]         *
+     * Now the vector should be: [10] | [2] | [3] | [40]             *
      *---------------------------------------------------------------*/
 
-    /* Retrieve the storage capacity. */
+    /* Check the number of stored items and the vector capacity. */
     int32_t iSize = pVec->size(pVec);
     assert(iSize == 4);
     int32_t iCap = pVec->capacity(pVec);
@@ -59,34 +92,30 @@ void usage_primitive()
     pVec->delete(pVec, 0);
 
     /*---------------------------------------------------------------*
-     * Now the vector should be: [20] | [30]                         *
+     * Now the vector should be: [2] | [3]                           *
      *---------------------------------------------------------------*/
 
     int32_t iNum = 0;
-#if __x86_64__
     pVec->get(pVec, &item, 0);
-    iNum += (int32_t)(int64_t)item;
+    iNum += ((Employ*)item)->iId;
     pVec->get(pVec, &item, 1);
-    iNum += (int32_t)(int64_t)item;
-#else
-    pVec->get(pVec, &item, 0);
-    iNum += (int32_t)item;
-    pVec->get(pVec, &item, 1);
-    iNum += (int32_t)item;
-#endif
+    iNum += ((Employ*)item)->iId;
+    iNum *= 20;
 
-    /* Resize the storage. */
+    /* Resize the vector storage. */
     pVec->resize(pVec, iNum);
 
-    /* Pop the item. */
+    /* Pop the rest items. */
     pVec->pop_back(pVec);
     pVec->pop_back(pVec);
 
     iSize = pVec->size(pVec);
     assert(iSize == 0);
     iCap = pVec->capacity(pVec);
-    assert(iCap == 50);
+    assert(iCap == 100);
 
+    /* You should deinitialize the DS after all the relevant tasks. */
     VectorDeinit(&pVec);
-    return;
+
+    return SUCC;
 }
