fe7cc4b1aebe2e0be0f50e321bc79f6ed1484069
ZSShen <andy780328@hotmail.com>
2015-07-25 03:27:36 -0700

    DS Renaming: Change BinHeap to BinaryHeap.

--- include/heap/binary_heap.h
+++ include/heap/binary_heap.h
@@ -45,24 +45,24 @@ typedef struct _BinaryHeap {
  *             Definition for the exported member operations                 *
  *===========================================================================*/
 /**
- * @brief The constructor for BinHeap.
+ * @brief The constructor for BinaryHeap.
  *
  * @param ppObj         The double pointer to the to be constructed heap
  *
  * @retval SUCC
  * @retval ERR_NOMEM    Insufficient memory for heap construction
  */
-int32_t BinHeapInit(BinHeap **ppObj);
+int32_t BinHeapInit(BinaryHeap **ppObj);
 
 /**
- * @brief The destructor for BinHeap.
+ * @brief The destructor for BinaryHeap.
  *
  * If the custom resource clean method is set, it also runs the clean method for
  * all the items.
  *
  * @param ppObj         The double pointer to the to be destructed heap
  */
-void BinHeapDeinit(BinHeap **ppObj);
+void BinHeapDeinit(BinaryHeap **ppObj);
 
 /**
  * @brief Push an item onto the heap.
@@ -70,14 +70,14 @@ void BinHeapDeinit(BinHeap **ppObj);
  * This function pushes an item onto the heap with the corresponding heap size
  * extension.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  * @param item          The designated item
  *
  * @retval SUCC
  * @retval ERR_NOINIT   Uninitialized container
  * @retval ERR_NOMEM    Insufficient memory for heap extension
  */
-int32_t BinHeapPush(BinHeap *self, Item item);
+int32_t BinHeapPush(BinaryHeap *self, Item item);
 
 /**
  * @brief Delete item from top of the heap.
@@ -85,13 +85,13 @@ int32_t BinHeapPush(BinHeap *self, Item item);
  * This function deletes item from top of the heap. If the custom resource clean
  * method is set, it also runs the clean method for the deleted item.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  *
  * @retval SUCC
  * @retval ERR_NOINIT   Uninitialized container
  * @retval ERR_IDX      Empty heap
  */
-int32_t BinHeapPop(BinHeap *self);
+int32_t BinHeapPop(BinaryHeap *self);
 
 /**
  * @brief Retrieve item from top of the heap.
@@ -100,7 +100,7 @@ int32_t BinHeapPop(BinHeap *self);
  * the item is returned by the second parameter. Otherwise, the error code is
  * returned and the second parameter is updated with NULL.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  * @param pItem         The pointer to the returned item
  *
  * @retval SUCC
@@ -108,38 +108,38 @@ int32_t BinHeapPop(BinHeap *self);
  * @retval ERR_IDX      Empty heap
  * @retval ERR_GET      Invalid parameter to store returned item
  */
-int32_t BinHeapTop(BinHeap *self, Item *pItem);
+int32_t BinHeapTop(BinaryHeap *self, Item *pItem);
 
 /**
  * @brief Return the number of stored items.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  *
  * @return              The number of items
  * @retval ERR_NOINIT   Uninitialized container
  */
-int32_t BinHeapSize(BinHeap *self);
+int32_t BinHeapSize(BinaryHeap *self);
 
 /**
  * @brief Set the custom item comparison method.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  * @param pFunc         The function pointer to the custom method
  *
  * @retval SUCC
  * @retval ERR_NOINIT   Uninitialized container
  */
-int32_t BinHeapSetCompare(BinHeap *self, int32_t (*pFunc) (Item, Item));
+int32_t BinHeapSetCompare(BinaryHeap *self, int32_t (*pFunc) (Item, Item));
 
 /**
  * @brief Set the custom item resource clean method.
  *
- * @param self          The pointer to BinHeap structure
+ * @param self          The pointer to BinaryHeap structure
  * @param pFunc         The function pointer to the custom method
  *
  * @retval SUCC
  * @retval ERR_NOINIT   Uninitialized container
  */
-int32_t BinHeapSetDestroy(BinHeap *self, void (*pFunc) (Item));
+int32_t BinHeapSetDestroy(BinaryHeap *self, void (*pFunc) (Item));
 
 #endif
