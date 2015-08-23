31636b7a4a55d741ff879965ed45fe3235b195fb
ZSShen <andy780328@hotmail.com>
2015-08-23 08:32:51 -0700

    TreeMap: Clean the error code definition.

--- include/util.h
+++ include/util.h
@@ -14,29 +14,26 @@
 /** The data structure is not initialized. */
 #define ERR_NOINIT      (-1)
 
-/** Insufficient memory for data structure expansion or reallocation. */
+/** Insufficient memory to extend data structure. */
 #define ERR_NOMEM       (-2)
 
 /** Invalid argument to index the data structure. */
 #define ERR_IDX         (-3)
 
-/** Duplicated key to insert associative data structures. */
-#define ERR_DUPKEY      (-4)
-
-/** No entry can be found with the given key from the associative data structures. */
-#define ERR_NODATA      (-5)
+/** No entry can be found in the associative data structures with the given key. */
+#define ERR_NODATA      (-4)
 
 /** Invalid argument to store the returned data from getter operation. */
-#define ERR_GET         (-6)
+#define ERR_GET         (-5)
 
 /** Invalid argument to specify the key size for hash data structures. */
-#define ERR_KEYSIZE     (-7)
+#define ERR_KEYSIZE     (-6)
 
-/** The data structure is modified during the structure traversal. */
-#define ERR_ITER        (-8)
+/** The data structure is modified during the traversal. */
+#define ERR_ITER        (-7)
 
 /** Fail to register the unit test function. */
-#define ERR_REG         (-9)
+#define ERR_REG         (-8)
 
 /** Iteration in progress. */
 #define CONTINUE		(1)
@@ -48,20 +45,16 @@
 #define NOKEY           (3)
 
 
-/** Item is the data type for the container element. */
+/** Item is the type for the elements of ordinary data structures. */
 typedef const void* Item;
 
-/** Key is the data type to index map-like containers. */
+/** Key is the type to index associative data structures. */
 typedef const void* Key;
 
-/** Value is the data type for the map-like element. */
+/** Value is the type for the data associating with a specific key. */
 typedef const void* Value;
 
-/** Entry is the data type which wraps key value pair to manipulate map-like
-    containers. */
-typedef const void* Entry;
-
-/** Pair is the real key value pair stored in the map-like containers. */
+/** Pair is the key value pair stored in associative data structures. */
 typedef struct _Pair {
     Key key;
     Value value;
