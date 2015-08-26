7e94f6812bcea23f8ff1b02091263bf7ae886172
ZSShen <andy780328@hotmail.com>
2015-08-26 10:21:59 -0700

    Update the document.

--- html/html/dir_fba38f648377aa56ae5d69782adf9e53.js
+++
@@ -1,4 +0,0 @@
-var dir_fba38f648377aa56ae5d69782adf9e53 =
-[
-    [ "hash.h", "hash_8h.html", "hash_8h" ]
-];
--- html/html/hash_8h.js
+++
@@ -1,5 +0,0 @@
-var hash_8h =
-[
-    [ "HashMurMur32", "hash_8h.html#acd38e3befee57c6125a9805343750c0a", null ],
-    [ "HashJenkins", "hash_8h.html#a42b579ec571d9cef757886ec6b6f190c", null ]
-];
