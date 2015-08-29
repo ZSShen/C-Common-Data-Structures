19b948e0b6919d2f6ac78247d8cf15b6d847bd76
ZSShen <andy780328@hotmail.com>
2015-08-29 07:28:34 -0700

    BloomFilter: Define the initial interface.

--- src/bloom_filter.c
+++ src/bloom_filter.c
@@ -6,8 +6,8 @@
  *===========================================================================*/
 
 struct _BloomFilterData {
- 	int32_t iSize_;
- 	char *aBit_;
+    int32_t iSize_;
+    char *aBit_;
 };
 
 
@@ -30,42 +30,42 @@ struct _BloomFilterData {
  *===========================================================================*/
 int32_t BloomFilterInit(BloomFilter **ppObj)
 {
-	/* Hopefully, we should let user specify:
-	   1. The number of expected elements for insertion.
-	   2. The acceptable FA rate.
+    /* Hopefully, we should let user specify:
+       1. The number of expected elements for insertion.
+       2. The acceptable FA rate.
 
-	   Based on this information, we create the internal bit array and prepare
-	   the relevant abount of hash functions. */
+       Based on this information, we create the internal bit array and prepare
+       the relevant abount of hash functions. */
 
-	#define TEMP_SIZE_BIT_ARRAY (1)
+    #define TEMP_SIZE_BIT_ARRAY (1)
 
-   	*ppObj = (BloomFilter*)malloc(sizeof(BloomFilter));
-   	if (!(*ppObj))
-   		return ERR_NOMEM;
+    *ppObj = (BloomFilter*)malloc(sizeof(BloomFilter));
+    if (!(*ppObj))
+        return ERR_NOMEM;
 
-   	BloomFilter *pObj = *ppObj;
-   	pObj->pData = (BloomFilterData*)malloc(sizeof(BloomFilterData));
-   	if (!(pObj->pData)) {
-   		free(*ppObj);
-   		*ppObj = NULL;
-		return ERR_NOMEM;
-   	}
+    BloomFilter *pObj = *ppObj;
+    pObj->pData = (BloomFilterData*)malloc(sizeof(BloomFilterData));
+    if (!(pObj->pData)) {
+        free(*ppObj);
+        *ppObj = NULL;
+        return ERR_NOMEM;
+    }
 
-   	BloomFilterData *pData = pObj->pData;
-   	pData->aBit_ = (char*)malloc(sizeof(char) * TEMP_SIZE_BIT_ARRAY);
-   	if (!(pData->aBit_)) {
-   		free(pObj->pData);
-   		free(*ppObj);
-   		*ppObj = NULL;
-   		return ERR_NOMEM;
-   	}
+    BloomFilterData *pData = pObj->pData;
+    pData->aBit_ = (char*)malloc(sizeof(char) * TEMP_SIZE_BIT_ARRAY);
+    if (!(pData->aBit_)) {
+        free(pObj->pData);
+        free(*ppObj);
+        *ppObj = NULL;
+        return ERR_NOMEM;
+    }
 
-   	pData->iSize_ = 0;
-   	pObj->insert = BloomFilterInsert;
-   	pObj->query = BloomFilterQuery;
-   	pObj->size = BloomFilterSize;
+    pData->iSize_ = 0;
+    pObj->insert = BloomFilterInsert;
+    pObj->query = BloomFilterQuery;
+    pObj->size = BloomFilterSize;
 
-   	return SUCC;
+    return SUCC;
 }
 
 void BloomFilterDeinit(BloomFilter **ppObj)
@@ -94,18 +94,18 @@ EXIT:
 
 int32_t BloomFilterInsert(BloomFilter *self, Key key, size_t size)
 {
-	CHECK_INIT(self);
+    CHECK_INIT(self);
     return SUCC;
 }
 
 int32_t BloomFilterQuery(BloomFilter *self, Key key, size_t size)
 {
-	CHECK_INIT(self);
+    CHECK_INIT(self);
     return SUCC;
 }
 
 int32_t BloomFilterSize(BloomFilter *self)
 {
-	CHECK_INIT(self);
+    CHECK_INIT(self);
     return self->pData->iSize_;
 }
