019fd70b801f35103592de72f3f06c448700ad23
ZSShen <andy780328@hotmail.com>
2015-09-09 07:15:50 -0700

    Update the document for trie.

---
+++ doc/gen/html/structTrie.js
@@ -0,0 +1,11 @@
+var structTrie =
+[
+    [ "pData", "structTrie.html#ad86e30f2c8af93d512ac8fae4b010567", null ],
+    [ "insert", "structTrie.html#a628c5be9f6fe8f05b27674186ad66ed0", null ],
+    [ "bulk_insert", "structTrie.html#af899f67cdbedff05c260c86d8643b758", null ],
+    [ "has_exact", "structTrie.html#abdf11b6a32d0a86b5fcaab416b88efa7", null ],
+    [ "has_prefix_as", "structTrie.html#addcb11a5310463fe4b927a55ae076c5a", null ],
+    [ "get_prefix_as", "structTrie.html#a942cb199cf6ea9e34cc2f597706e511e", null ],
+    [ "delete", "structTrie.html#afd3d2d2ca0aaeb0a9c0e4b12343e4731", null ],
+    [ "size", "structTrie.html#aeed6ca5612d5cc709f37948623ae5b51", null ]
+];
