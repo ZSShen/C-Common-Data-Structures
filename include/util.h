f6314bd7107ad145ff2841916d74d1d81cc2df06
ZSShen <andy780328@hotmail.com>
2016-06-26 09:29:10 -0700

    Prefer static constant over macro constant.

--- include/util.h
+++ include/util.h
@@ -9,40 +9,40 @@
 #include <assert.h>
 
 /** The function is finished gracefully. */
-#define SUCC            (0)
+static const int32_t SUCC = 0;
 
 /** The data structure is not initialized. */
-#define ERR_NOINIT      (-1)
+static const int32_t ERR_NOINIT = -1;
 
 /** Insufficient memory to extend data structure. */
-#define ERR_NOMEM       (-2)
+static const int32_t ERR_NOMEM = -2;
 
 /** Invalid argument to index the data structure. */
-#define ERR_IDX         (-3)
+static const int32_t ERR_IDX = -3;
 
 /** No entry can be found in the associative data structures with the given key. */
-#define ERR_NODATA      (-4)
+static const int32_t ERR_NODATA = -4;
 
 /** Invalid argument to store the returned data from getter operation. */
-#define ERR_GET         (-5)
+static const int32_t ERR_GET = -5;
 
 /** Invalid argument to specify the key size for hash data structures. */
-#define ERR_KEYSIZE     (-6)
+static const int32_t ERR_KEYSIZE = -6;
 
 /** The data structure is modified during the traversal. */
-#define ERR_ITER        (-7)
+static const int32_t ERR_ITER = -7;
 
 /** Fail to register the unit test function. */
-#define ERR_REG         (-8)
+static const int32_t ERR_REG = -8;
 
 /** Iteration in progress. */
-#define CONTINUE		(1)
+static const int32_t CONTINUE = 1;
 
 /** Iteration terminated at the end of the data structure */
-#define END             (2)
+static const int32_t END = 2;
 
 /** The designated key cannot be found in the map */
-#define NOKEY           (3)
+static const int32_t NOKEY = 3;
 
 
 /** Item is the type for the elements of ordinary data structures. */
