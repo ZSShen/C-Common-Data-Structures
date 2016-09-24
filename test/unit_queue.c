5b4712b8387232c71d7b2bce1d45d8d2ed3f59b6
ZSShen <andy780328@hotmail.com>
2016-09-24 02:51:40 -0700

    Queue: Complere the refined implementation, unit test, and demo program.

--- test/unit_queue.c
+++ test/unit_queue.c
@@ -3,34 +3,185 @@
 #include "CUnit/Basic.h"
 
 
-#define SIZE_SMALL_TEST     (32)
-#define MASK_YEAR           (100)
-#define MASK_LEVEL          (200)
+static const int SIZE_SML_TEST = 32;
 
-typedef struct Employ_ {
-    int8_t cYear;
-    int8_t cLevel;
-    int32_t iId;
-} Employ;
+typedef struct Tuple_ {
+    int first;
+    int second;
+} Tuple;
 
 
-int32_t AddSuite();
-void TestManipulate();
-void TestBoundary();
-void DestroyItem(Item);
-
-
-int32_t main()
+/*-----------------------------------------------------------------------------*
+ *                  The utilities for element resource clean                   *
+ *-----------------------------------------------------------------------------*/
+void CleanObject(void* element)
 {
-    int32_t rc = SUCC;
+    free(element);
+}
+
+
+/*-----------------------------------------------------------------------------*
+ *            Unit tests relevant to basic structure verification              *
+ *-----------------------------------------------------------------------------*/
+void TestNewDelete()
+{
+    Queue* queue;
+    CU_ASSERT((queue = QueueInit()) != NULL);
+
+    /* Enlarge the queue size to test the destructor. */
+    int i;
+    for (i = 0 ; i < SIZE_SML_TEST ; ++i)
+        CU_ASSERT(queue->push(queue, (void*)(intptr_t)i) == true);
+
+    QueueDeinit(queue);
+}
+
+void TestOrderNumerics()
+{
+    Queue* queue = QueueInit();
+
+    /* Boundary check. */
+    void *element;
+    CU_ASSERT(queue->pop(queue) == false);
+    CU_ASSERT(queue->front(queue, &element) == false);
+    CU_ASSERT(queue->back(queue, &element) == false);
+
+    int i;
+    for (i = 0 ; i < SIZE_SML_TEST ; ++i)
+        CU_ASSERT(queue->push(queue, (void*)(intptr_t)i) == true);
+    CU_ASSERT_EQUAL(queue->size(queue), SIZE_SML_TEST);
+
+    /* Remove the elements ranging from 0 to 15. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i)
+        CU_ASSERT(queue->pop(queue) == true);
+
+    /* Re-push the removed elements. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i)
+        CU_ASSERT(queue->push(queue, (void*)(intptr_t)i) == true);
+
+    /* Check the queue order. */
+    CU_ASSERT(queue->front(queue, &element) == true);
+    CU_ASSERT_EQUAL((int)(intptr_t)element, SIZE_SML_TEST >> 1);
+    CU_ASSERT(queue->back(queue, &element) == true);
+    CU_ASSERT_EQUAL((int)(intptr_t)element, (SIZE_SML_TEST >> 1) - 1);
+
+    /* Push another set of elements. */
+    for (i = SIZE_SML_TEST ; i < SIZE_SML_TEST << 1 ; ++i)
+        CU_ASSERT(queue->push(queue, (void*)(intptr_t)i) == true);
+
+    /* Remove the elements ranging from 16 to 31. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i)
+        CU_ASSERT(queue->pop(queue) == true);
+
+    /* Check the queue order. */
+    CU_ASSERT(queue->front(queue, &element) == true);
+    CU_ASSERT_EQUAL((int)(intptr_t)element, 0);
+    CU_ASSERT(queue->back(queue, &element) == true);
+    CU_ASSERT_EQUAL((int)(intptr_t)element, (SIZE_SML_TEST << 1) - 1);
+
+    QueueDeinit(queue);
+}
+
+
+/*-----------------------------------------------------------------------------*
+ *              Unit tests relevant to complex data maintenance                *
+ *-----------------------------------------------------------------------------*/
+void TestOrderObjects()
+{
+    Queue* queue = QueueInit();
+    queue->set_clean(queue, CleanObject);
+
+    int i;
+    for (i = 0 ; i < SIZE_SML_TEST ; ++i) {
+        Tuple* tuple = (Tuple*)malloc(sizeof(Tuple));
+        tuple->first = i;
+        tuple->second = -i;
+        CU_ASSERT(queue->push(queue, tuple) == true);
+    }
+    CU_ASSERT_EQUAL(queue->size(queue), SIZE_SML_TEST);
+
+    /* Remove the elements ranging from 0 to 15. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i)
+        CU_ASSERT(queue->pop(queue) == true);
+
+    /* Re-push the removed elements. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i) {
+        Tuple* tuple = (Tuple*)malloc(sizeof(Tuple));
+        tuple->first = i;
+        tuple->second = -i;
+        CU_ASSERT(queue->push(queue, tuple) == true);
+    }
+
+    /* Check the queue order. */
+    void* element;
+    CU_ASSERT(queue->front(queue, &element) == true);
+    CU_ASSERT_EQUAL(((Tuple*)element)->first, SIZE_SML_TEST >> 1);
+    CU_ASSERT(queue->back(queue, &element) == true);
+    CU_ASSERT_EQUAL(((Tuple*)element)->first, (SIZE_SML_TEST >> 1) - 1);
+
+    /* Push another set of elements. */
+    for (i = SIZE_SML_TEST ; i < SIZE_SML_TEST << 1 ; ++i) {
+        Tuple* tuple = (Tuple*)malloc(sizeof(Tuple));
+        tuple->first = i;
+        tuple->second = -i;
+        CU_ASSERT(queue->push(queue, tuple) == true);
+    }
+
+    /* Remove the elements ranging from 16 to 31. */
+    for (i = 0 ; i < SIZE_SML_TEST >> 1 ; ++i)
+        CU_ASSERT(queue->pop(queue) == true);
+
+    /* Check the queue order. */
+    CU_ASSERT(queue->front(queue, &element) == true);
+    CU_ASSERT_EQUAL(((Tuple*)element)->first, 0);
+    CU_ASSERT(queue->back(queue, &element) == true);
+    CU_ASSERT_EQUAL(((Tuple*)element)->first, (SIZE_SML_TEST << 1) - 1);
+
+    QueueDeinit(queue);
+}
+
+
+/*-----------------------------------------------------------------------------*
+ *                       The driver for Queue unit test                        *
+ *-----------------------------------------------------------------------------*/
+bool AddSuite()
+{
+    {
+        CU_pSuite suite = CU_add_suite("Structure Verification", NULL, NULL);
+        if (!suite)
+            return false;
+
+        CU_pTest unit = CU_add_test(suite, "Queue New and Delete", TestNewDelete);
+        if (!unit)
+            return false;
+
+        unit = CU_add_test(suite, "Numerics Push, Pop, and Get", TestOrderNumerics);
+        if (!unit)
+            return false;
+    }
+    {
+        CU_pSuite suite = CU_add_suite("Complex Data Maintenance", NULL, NULL);
+        if (!suite)
+            return false;
+
+        CU_pTest unit = CU_add_test(suite, "Objects Push, Pop, and Get", TestOrderObjects);
+        if (!unit)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int rc = 0;
 
     if (CU_initialize_registry() != CUE_SUCCESS) {
         rc = CU_get_error();
         goto EXIT;
     }
 
-    /* Register the test suite for queue structure verification. */
-    if (AddSuite() != SUCC) {
+    /* Register the test suite for Queue structure verification. */
+    if (AddSuite() == false) {
         rc = CU_get_error();
         goto CLEAN;
     }
@@ -43,135 +194,4 @@ CLEAN:
     CU_cleanup_registry();
 EXIT:
     return rc;
-}
-
-int32_t AddSuite()
-{
-    int32_t rc = SUCC;
-
-    CU_pSuite pSuite = CU_add_suite("Structure Verification", NULL, NULL);
-    if (!pSuite) {
-        rc = ERR_REG;
-        goto EXIT;
-    }
-
-    char *szMsg = "Combination with insertion, deletion, and order checking.";
-    CU_pTest pTest = CU_add_test(pSuite, szMsg, TestManipulate);
-    if (!pTest)
-        rc = ERR_REG;
-
-    pTest = CU_add_test(pSuite, "Boundary Testing", TestBoundary);
-    if (!pTest)
-        rc = ERR_REG;
-
-EXIT:
-    return rc;
-}
-
-void DestroyItem(Item item)
-{
-    free((Employ*)item);
-}
-
-void TestManipulate()
-{
-    Queue *pQueue;
-    CU_ASSERT(QueueInit(&pQueue) == SUCC);
-    CU_ASSERT(pQueue->set_destroy(pQueue, DestroyItem) == SUCC);
-
-    /* Insert the initial data set. */
-    int32_t iIdx;
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST ; iIdx++) {
-        Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
-        pEmploy->cYear = iIdx % MASK_YEAR;
-        pEmploy->cLevel = iIdx % MASK_LEVEL;
-        pEmploy->iId = iIdx;
-        CU_ASSERT(pQueue->push(pQueue, (Item)pEmploy) == SUCC);
-    }
-    CU_ASSERT_EQUAL(pQueue->size(pQueue), SIZE_SMALL_TEST);
-
-    /* Delete the first half of the data set. */
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST / 2 ; iIdx++)
-        CU_ASSERT(pQueue->pop(pQueue) == SUCC);
-
-    /* Reinsert the deleted data set. */
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST / 2 ; iIdx++) {
-        Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
-        pEmploy->cYear = iIdx % MASK_YEAR;
-        pEmploy->cLevel = iIdx % MASK_LEVEL;
-        pEmploy->iId = iIdx;
-        CU_ASSERT(pQueue->push(pQueue, (Item)pEmploy) == SUCC);
-    }
-
-    /* Check the queue order. */
-    Item item;
-    CU_ASSERT(pQueue->front(pQueue, &item) == SUCC);
-    CU_ASSERT_EQUAL(SIZE_SMALL_TEST / 2, ((Employ*)item)->iId);
-    CU_ASSERT(pQueue->back(pQueue, &item) == SUCC);
-    CU_ASSERT_EQUAL(SIZE_SMALL_TEST / 2 - 1, ((Employ*)item)->iId);
-
-    /* Insert another data set. */
-    for (iIdx = SIZE_SMALL_TEST ; iIdx < SIZE_SMALL_TEST * 2 ; iIdx++) {
-        Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
-        pEmploy->cYear = iIdx % MASK_YEAR;
-        pEmploy->cLevel = iIdx % MASK_LEVEL;
-        pEmploy->iId = iIdx;
-        CU_ASSERT(pQueue->push(pQueue, (Item)pEmploy) == SUCC);
-    }
-
-    /* Delete the second quarter of the data set. */
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST / 2 ; iIdx++)
-        CU_ASSERT(pQueue->pop(pQueue) == SUCC);
-
-    /* Check the queue order. */
-    CU_ASSERT(pQueue->front(pQueue, &item) == SUCC);
-    CU_ASSERT_EQUAL(0, ((Employ*)item)->iId);
-    CU_ASSERT(pQueue->back(pQueue, &item) == SUCC);
-    CU_ASSERT_EQUAL(SIZE_SMALL_TEST * 2 - 1, ((Employ*)item)->iId);
-
-    QueueDeinit(&pQueue);
-}
-
-void TestBoundary()
-{
-    Queue *pQueue;
-    CU_ASSERT(QueueInit(&pQueue) == SUCC);
-    CU_ASSERT(pQueue->set_destroy(pQueue, DestroyItem) == SUCC);
-
-    /* Pop the empty queue. */
-    CU_ASSERT(pQueue->pop(pQueue) == ERR_IDX);
-
-    /* Retrieve the items from the empty queue. */
-    Item item;
-    CU_ASSERT(pQueue->front(pQueue, &item) == ERR_IDX);
-    CU_ASSERT_EQUAL(item, NULL);
-    CU_ASSERT(pQueue->back(pQueue, &item) == ERR_IDX);
-    CU_ASSERT_EQUAL(item, NULL);
-
-    /* Insert the initial data set. */
-    int iIdx;
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST ; iIdx++) {
-        Employ *pEmploy = (Employ*)malloc(sizeof(Employ));
-        pEmploy->cYear = iIdx % MASK_YEAR;
-        pEmploy->cLevel = iIdx % MASK_LEVEL;
-        pEmploy->iId = iIdx;
-        CU_ASSERT(pQueue->push(pQueue, (Item)pEmploy) == SUCC);
-    }
-
-    /* Delete all the data. */
-    for (iIdx = 0 ; iIdx < SIZE_SMALL_TEST ; iIdx++)
-        CU_ASSERT(pQueue->pop(pQueue) == SUCC);
-
-    CU_ASSERT(pQueue->pop(pQueue) == ERR_IDX);
-    CU_ASSERT(pQueue->front(pQueue, &item) == ERR_IDX);
-    CU_ASSERT_EQUAL(item, NULL);
-    CU_ASSERT(pQueue->back(pQueue, &item) == ERR_IDX);
-    CU_ASSERT_EQUAL(item, NULL);
-    CU_ASSERT_EQUAL(pQueue->size(pQueue), 0);
-
-    /* Invalid parameter to retrieve queue data. */
-    CU_ASSERT(pQueue->front(pQueue, NULL) == ERR_GET);
-    CU_ASSERT(pQueue->back(pQueue, NULL) == ERR_GET);
-
-    QueueDeinit(&pQueue);
 }
