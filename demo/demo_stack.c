1ab56c39fcbdcc4c8676f69fcdf4c30aff5e6528
tlfu <0050211@gmail.com>
2019-01-09 21:20:26 +0800

    demo_stack.c: Add cpp style demo functions

--- demo/demo_stack.c
+++ demo/demo_stack.c
@@ -85,9 +85,86 @@ void ManipulateObjects()
     StackDeinit(stack);
 }
 
+void ManipulateNumericsCppStyle()
+{
+    /* We should initialize the container before any operations. */
+    Stack* stack = StackInit();
+
+    /* Push elements to the stack. */
+    stack->push(stack, (void*)3);
+    stack->push(stack, (void*)2);
+    stack->push(stack, (void*)1);
+
+    /* Check the number of stored elements. */
+    assert(StackSize(stack) == 3);
+
+    /* Retrieve the stack top element and remove it. */
+    void* element;
+    stack->top(stack, &element);
+    assert((int)(intptr_t)element == 1);
+    stack->pop(stack);
+
+    stack->top(stack, &element);
+    assert((int)(intptr_t)element == 2);
+    stack->pop(stack);
+
+    stack->top(stack, &element);
+    assert((int)(intptr_t)element == 3);
+    stack->pop(stack);
+
+    StackDeinit(stack);
+}
+
+void ManipulateObjectsCppStyle()
+{
+    /* We should initialize the container before any operations. */
+    Stack* stack = StackInit();
+    StackSetClean(stack, CleanObject);
+
+    /* Push elements to the stack. */
+    Tuple* tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 3;
+    tuple->second = -3;
+    // StackPush(stack, tuple);
+    stack->push(stack, tuple);
+
+    tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 2;
+    tuple->second = -2;
+    stack->push(stack, tuple);
+
+    tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 1;
+    tuple->second = -1;
+    stack->push(stack, tuple);
+
+    /* Check the number of stored elements. */
+    assert(StackSize(stack) == 3);
+
+    /* Retrieve the stack top element and remove it. */
+    void* element;
+    // StackTop(stack, &element);
+    stack->top(stack, &element);
+    assert(((Tuple*)element)->first == 1);
+    // StackPop(stack);
+    stack->pop(stack);
+
+    stack->top(stack, &element);
+    assert(((Tuple*)element)->first == 2);
+    stack->pop(stack);
+
+    stack->top(stack, &element);
+    assert(((Tuple*)element)->first == 3);
+    stack->pop(stack);
+
+    StackDeinit(stack);
+}
+
 int main()
 {
     ManipulateNumerics();
     ManipulateObjects();
+    ManipulateNumericsCppStyle();
+    ManipulateObjectsCppStyle();
     return 0;
 }
