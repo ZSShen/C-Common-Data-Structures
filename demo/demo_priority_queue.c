1c8cef9e10466c0b03faf39c1c7aa2f36e7d8e57
tlfu <0050211@gmail.com>
2019-01-11 20:12:49 +0800

    demo_priority_queue.c: changes all four demo functions into ManipulateNumerics(), ManipulateObjects(), ManipulateNumericsCppStyle() and ManipulateObjectsCppStyle()

--- demo/demo_priority_queue.c
+++ demo/demo_priority_queue.c
@@ -38,6 +38,88 @@ void ManipulateNumerics()
     queue->set_compare(queue, CompareNumerics);
 
     /* Push integer elements to the queue. */
+    PriorityQueuePush(queue, (void*)(intptr_t)4);
+    PriorityQueuePush(queue, (void*)(intptr_t)3);
+    PriorityQueuePush(queue, (void*)(intptr_t)2);
+    PriorityQueuePush(queue, (void*)(intptr_t)1);
+
+    /* Pop elements from the queue and check the top elements. */
+    void* elem;
+    PriorityQueueTop(queue, &elem);
+    assert((int)(intptr_t)elem == 1);
+    PriorityQueuePop(queue);
+
+    PriorityQueueTop(queue, &elem);
+    assert((int)(intptr_t)elem == 2);
+    PriorityQueuePop(queue);
+
+    PriorityQueueTop(queue, &elem);
+    assert((int)(intptr_t)elem == 3);
+    PriorityQueuePop(queue);
+
+    /* Check the number of stored elements. */
+    unsigned size = PriorityQueueSize(queue);
+    assert(size == 1);
+
+    PriorityQueueDeinit(queue);
+}
+
+void ManipulateObjects()
+{
+    /* We should initialize the container before any operations. */
+    PriorityQueue* queue = PriorityQueueInit();
+    queue->set_compare(queue, CompareObjects);
+    queue->set_clean(queue, CleanObject);
+
+    /* Push integer elements to the queue. */
+    Tuple* tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 4;
+    tuple->second = -4;
+    PriorityQueuePush(queue, tuple);
+
+    tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 3;
+    tuple->second = -3;
+    PriorityQueuePush(queue, tuple);
+
+    tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 2;
+    tuple->second = -2;
+    PriorityQueuePush(queue, tuple);
+
+    tuple = (Tuple*)malloc(sizeof(Tuple));
+    tuple->first = 1;
+    tuple->second = -1;
+    PriorityQueuePush(queue, tuple);
+
+    /* Pop elements from the queue and check the top elements. */
+    void* elem;
+    PriorityQueueTop(queue, &elem);
+    assert(((Tuple*)elem)->first == 4);
+    PriorityQueuePop(queue);
+
+    PriorityQueueTop(queue, &elem);
+    assert(((Tuple*)elem)->first == 3);
+    PriorityQueuePop(queue);
+
+    PriorityQueueTop(queue, &elem);
+    assert(((Tuple*)elem)->first == 2);
+    PriorityQueuePop(queue);
+
+    /* Check the number of stored elements. */
+    unsigned size = PriorityQueueSize(queue);
+    assert(size == 1);
+
+    PriorityQueueDeinit(queue);
+}
+
+void ManipulateNumericsCppStyle()
+{
+    /* We should initialize the container before any operations. */
+    PriorityQueue* queue = PriorityQueueInit();
+    queue->set_compare(queue, CompareNumerics);
+
+    /* Push integer elements to the queue. */
     queue->push(queue, (void*)(intptr_t)4);
     queue->push(queue, (void*)(intptr_t)3);
     queue->push(queue, (void*)(intptr_t)2);
@@ -64,7 +146,7 @@ void ManipulateNumerics()
     PriorityQueueDeinit(queue);
 }
 
-void ManipulateObjects()
+void ManipulateObjectsCppStyle()
 {
     /* We should initialize the container before any operations. */
     PriorityQueue* queue = PriorityQueueInit();
@@ -117,5 +199,7 @@ int main()
 {
     ManipulateNumerics();
     ManipulateObjects();
+    ManipulateNumericsCppStyle();
+    ManipulateObjectsCppStyle();
     return 0;
 }
